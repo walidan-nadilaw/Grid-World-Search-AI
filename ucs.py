00cebd8de02588e77f4bc7cab3aad6b2a329cf2a
hanya-manusia <syafiqrmuhammad@gmail.com>
2025-11-25 20:05:35 +0700

    feat: ucs succesfully implemented

--- ucs.py
+++ ucs.py
@@ -1,40 +1,46 @@
-from sys import *
 from general import *
 
 def UCS(row, col, grid, start, goal):
     
-    enqueuedBefore = []
-    for i in range(row):
-        for j in range(col):
-            enqueuedBefore[i][j] = 0
+    # simpan best-known g-cost untuk tiap sel
+    enqueuedWeight = [[float('inf') for _ in range(col)] for _ in range(row)]
     
     explored = []
-    # AccWeightMatrix = []
-    # for i in range(row):
-    #     rowmat = []
-    #     for j in range(col):
-    #         rowmat.append(sys.maxsize)
-    
     priorityQueue = MinHeap_NumbVal()
     
+    enqueuedWeight[start[0]][start[1]] = 0
     priorityQueue.insert([[start[0], start[1]], 0, [-1, -1]])
-    explore = priorityQueue.delete(priorityQueue.getMin())
-    enqueuedBefore[explore[0][0]][explore[0][1]] = 1
+    explore = priorityQueue.popMin()
+
+    # jika queue kosong atau sentinel, keluar
+    if explore == [[-1, -1], -1, [-1, -1]]:
+        print("No path found")
+        return
 
     while(explore != [[-1,-1], -1, [-1,-1]] and explore[0] != goal):
         for neighbor in getNeighbor(explore[0][0], explore[0][1], row, col, grid):
-            if (grid[neighbor[0]][neighbor[1]] == '#' or enqueuedBefore[neighbor[0]][neighbor[1]] == 1): continue
+            if grid[neighbor[0]][neighbor[1]] == '#': 
+                continue
             
-            neighbor_d = explore[1] + eightD(explore[0], neighbor[0])
-            # if (neighbor_d >= enqueuedWeight[neighbor[0], neighbor[1]]): continue
-    
-            priorityQueue.insert([[neighbor[0], neighbor[1]], neighbor_d, [explore[0][0], explore[0][1]]])
-            enqueuedBefore[neighbor[0]][neighbor[1]] = 1
+            neighbor_d = explore[1] + eightD(explore[0], neighbor)
+            # hanya enqueue jika cost lebih baik
+            if neighbor_d < enqueuedWeight[neighbor[0]][neighbor[1]]:
+                enqueuedWeight[neighbor[0]][neighbor[1]] = neighbor_d
+                priorityQueue.insert([[neighbor[0], neighbor[1]], neighbor_d, [explore[0][0], explore[0][1]]])
         
         explored.append(explore)
-        explore = priorityQueue.delete(priorityQueue.getMin())
-        enqueuedBefore[explore[0][0]][explore[0][1]] = 1
-
+        explore = priorityQueue.popMin()
+        if explore == [[-1, -1], -1, [-1, -1]]:
+            break
     
+    # jika goal dipop, tambahkan ke explored agar backtrack bekerja
+    if explore != [[-1, -1], -1, [-1, -1]] and explore[0] == goal:
+        explored.append(explore)
 
-        
+    printUCS(explored, goal)
+
+def printUCS(explored, goal):
+    if not explored or explored[-1][0] != goal:
+        print("No path found")
+    else:
+        printBacktrack(explored)
