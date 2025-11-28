02ac4f162421c822c3f4a39c3857fdd556342d7c
hanya-manusia <syafiqrmuhammad@gmail.com>
2025-11-28 11:44:35 +0700

    feat: add comments on ucs.py and repair some enqueued weight

--- source/main.py
+++ source/main.py
@@ -3,25 +3,25 @@ from general import inputGrid, printPath
 
 if __name__ == "__main__":
 
-    # file Membaca input grid dari file .txt
+    # Membaca input grid dari file .txt
     row, col, grid, start, goal = inputGrid('dataset')
 
-    # Print the inputed grid
+    # Mencetak grid yang dibaca
     for i in range(row):
         for j in range(col):
             print(grid[i][j], end=' ')
         print()
 
     # UCS
-    # Calling the function
+    # Memanggil UCS
     nExp_UCS, pathCost_UCS, path_UCS, time_UCS = UCS(row, col, grid, start, goal)
 
-    # Printing the result
+    # Mencetak hasil
     print("UCS explore\t:", nExp_UCS)
     print("UCS cost\t:", pathCost_UCS[1])
     
     print("UCS path\t: ", end='')
-    if(type(path_UCS) is str): print(path_UCS)
-    else: printPath(path_UCS)
+    if(type(path_UCS) is str): print(path_UCS)  # ketika tidak ada path dari start menuju goal
+    else: printPath(path_UCS)                   # ketika ada path dari start menuju goal
     
     print(f"UCS time\t: {time_UCS:.6f} seconds")
--- source/ucs.py
+++ source/ucs.py
@@ -2,26 +2,40 @@ from general import *
 import time
 
 def UCS(row, col, grid, start, goal):
-    # 
+    # Menyimpan waktu dimana fungsi mulai berjalan
     start_time = time.perf_counter()
     
-    # simpan best-known g-cost untuk tiap sel
-    enqueuedWeight = [[float('inf') for _ in range(col)] for _ in range(row)]
+    # Menyimpan bobot akumulatif(? lupa kata yang tepat) terbaik untuk setiap node
+    enqueuedWeight = [[float('inf') for _ in range(col)] for _ in range(row)] # matrix row x col
+    for i in range(row): 
+        for j in range(col): 
+            enqueuedWeight[i][j] = -1
+    # -1 menyatakan node belum dienqueue
     
-    explored = []
+    # List untuk menyimpan semua node yang sudah dieksplorasi
+    explored = []  # format: [[row node, col node], bobot akumulatif, [row prev, col prev], urutan queue]
+
+    # Priority queue (PQ) untuk menyimpan daftar node yang sudah dienqueue tapi belom divisit
     priorityQueue = MinHeap_NumbVal()
     
-    enqueuedWeight[start[0]][start[1]] = 0
-    priorityQueue.insert([[start[0], start[1]], 0, [-1, -1]])
-    explore = priorityQueue.popMin()
+    # inisialisasi algoritma ucs
+    enqueuedWeight[start[0]][start[1]] = 0      # menyatakan bahwa bobot akumulasi ke start adalah 0
+    priorityQueue.insert([[start[0], start[1]], 0, [-1, -1]]) # memasukkan node start ke PQ
+    explore = priorityQueue.popMin()            # mengvisit node terkecil pada PQ, disini berarti node start
 
+    # Error handling ketika node start tidak ada
     # jika queue kosong atau sentinel, keluar
     if explore == [[-1, -1], -1, [-1, -1]]:
         print("No path found")
         return
 
+    # Selama masih ada node yang bisa divisit dan node yang divisit bukan node goal, enqueue tetangga
     while(explore != [[-1,-1], -1, [-1,-1]] and explore[0] != goal):
+        
+        # iterasi terhadap semua tetangga dari node yang sedang diexplore
         for neighbor in getNeighbor(explore[0][0], explore[0][1], row, col, grid):
+            
+            # jika tetangga berupa '#' atau dinding, maka tidak bisa diexplore
             if grid[neighbor[0]][neighbor[1]] == '#': 
                 continue
             
